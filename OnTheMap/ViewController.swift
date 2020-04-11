d788540ea6036100e89d7d54c05c39de14aa188d
Jasjeev <jasjeev4@gmail.com>
2020-04-11 19:50:25 +0530

    Initial commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by JASJEEV on 4/11/20.
+//  Copyright © 2020 Lorgarithmic Science. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
